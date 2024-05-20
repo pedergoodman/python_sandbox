d1b03f1106f93355289c2f8117bf18dc18456cf3
Peder <116122618+pedergoodman@users.noreply.github.com>
2024-05-20 15:34:58 -0500

    add variables

--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -13,3 +13,23 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+# * you don't need to add a "const or let" keyword to declare a variable
+
+# * main types of variables
+# x =1 # int
+# y = 2.5 # float
+# name = 'John' # string
+# isCool = True # bool
+
+# * Multiple assignment
+x, y, name, cool = (1, 2.5, 'John', True)
+
+print(x, y, name, cool)
+
+xType = type(x)
+xString = str(x)
+
+print(xType)
+print(xString)
+print(int(y))
