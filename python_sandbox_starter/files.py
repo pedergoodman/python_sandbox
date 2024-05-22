5c1664dcf856ce0af6ccc4411a5804f144a2511f
Peder <116122618+pedergoodman@users.noreply.github.com>
2024-05-22 11:54:50 -0500

    files and json

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -1,1 +1,37 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+#open a file
+
+# open creates a file
+# 'w' is for write
+# 'r' is for read
+# 'a' is for append
+# 'r+' is for read and write
+# 'w+' is for writing and reading
+myfile = open('myfile.txt', 'w')
+
+# get some info
+print('Name:', myfile.name)
+print('Readable:', myfile.readable())
+print('Is Closed:', myfile.closed)
+print('Opening Mode:', myfile.mode)
+
+# write to file
+myfile.write('I love Python')
+myfile.write(' and Javascript')
+myfile.close()
+
+print('Is Closed:', myfile.closed)
+
+
+# append to file
+# a is for append
+myfile = open('myfile.txt', 'a')
+myfile.write('\nI also like Typescript')
+myfile.close()
+
+# read from file
+# r+ is for read and write
+myfile = open('myfile.txt', 'r+')
+text = myfile.read(100) # read first 100 characters
+print(text)
--- python_sandbox_starter/py_json.py
+++ python_sandbox_starter/py_json.py
@@ -1,1 +1,26 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+# pares dictionary into JSON
+# parse JSON into a dictionary
+
+import json
+
+#sample JSON
+userJSON = '{"first_name": "John", "last_name": "Doe", "age": 30}'
+
+user = json.loads(userJSON)
+
+print(user)
+print(user['first_name'])
+
+car = {'make': 'Ford', 'model': 'Mustang', 'year': 1970}
+
+carJSON = json.dumps(car)
+
+print(carJSON)
+print(type(user))
+
+print(f"User JSON: {userJSON}")
+print(f"User dictionary: {user}")
+print(f"Car JSON: {carJSON}")
+print(f"Car dictionary: {car}")
+
